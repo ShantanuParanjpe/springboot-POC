326026af1a01b3bf8e2a28ea0dcb8987791d8490
Your Name <you@example.com>
2026-04-14 20:00:14 +0100

    removed public from MainTest Class

--- springboot/demo/src/test/java/com/example/MainTest.java
+++ springboot/demo/src/test/java/com/example/MainTest.java
@@ -3,7 +3,7 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MainTest {
+ class MainTest {
 
     @Test
     void testHello() {
