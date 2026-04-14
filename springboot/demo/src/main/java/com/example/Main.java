2381184832037a899ce1946a298e15868910bd0c
Your Name <you@example.com>
2026-04-14 20:36:19 +0100

    removed Main.java

--- springboot/demo/src/main/java/com/example/Main.java
+++ springboot/demo/src/main/java/com/example/Main.java
@@ -5,13 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
-public class Main {
-
-    public static void main(String[] args) {
-         SpringApplication.run(Main.class, args);
-    }
+public class HelloController {
 
     @GetMapping("/")
     public String hello() {
